22a642642bda3f55ff010f5c3f7ae7a11b11588c
Jeffrey Burrows <jeffreyb@Jeffreys-MacBook-Pro.local>
2018-03-16 14:24:40 -0700

    solved 08 by cheating

--- 07_10001st_prime.py
+++ 07_10001st_prime.py
@@ -1,5 +1,13 @@
+# coding=utf-8
 """
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 What is the 10 001st prime number?
 """
+
+from sympy import primerange
+
+list = ([i for i in primerange(2, 200000)])
+
+print(len(list))
+print(list[10000])
