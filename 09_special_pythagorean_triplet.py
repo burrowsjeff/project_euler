6b61b126a2fa11742af11f8cd7c85b6849d7121f
Jeff Burrows <burrows.jeff@gmail.com>
2018-03-17 09:25:04 -0700

    First failed attempt for 09

--- 09_special_pythagorean_triplet.py
+++ 09_special_pythagorean_triplet.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
@@ -7,3 +8,15 @@ For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
+
+from math import sqrt
+
+a = 1
+
+while a <= 331:
+	for b in range(a+1,332):
+		if (a+b+(sqrt((a+b)^2))) == 1000:
+			print('A =', a, 'B =', b, 'C =', (sqrt((a+b)^2)))
+			break
+	print(a)
+	a += 1
